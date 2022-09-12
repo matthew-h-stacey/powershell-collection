689273a8b3ed1673cda2d0f9fa266f89bd391a95
matthew-h-stacey <matthew.h.stacey@outlook.com>
2022-09-12 13:53:39 -0400

    New script; Add support for array

--- ExchangeOnline/Remove-MobileDeviceData.ps1
+++ ExchangeOnline/Remove-MobileDeviceData.ps1
@@ -1,6 +1,6 @@
 param
 (   
-    [Parameter(Mandatory = $true)] [string] $UserPrincipalName, # UPN of the user to clear mobile data and partnerships from
+    [Parameter(Mandatory = $true)] [string[]] $UserPrincipalName, # UPN of the user to clear mobile data and partnerships from
     [Parameter(Mandatory = $false)] [switch] $NativeAppOnly # Optional, use this parameter to ONLY clear native (non-Outlook) mobile app data/partnerships
 )
 function Connect-Modules {
@@ -14,53 +14,60 @@ function Connect-Modules {
 }
 function Remove-MobileDeviceData {
 
-    Write-Host "[EXO] Attempting to locate mobile devices to delete data from..."
-    
-    if ($NativeAppOnly) {
-        $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | ? { $_.DeviceModel -notLike "*outlook*" }
-    }
-    else {
-        $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | ? { $_.DeviceModel -notLike "*outlook*" }
-    }
-    
-    if ($null -eq $userPhones) {
-        Write-Host "[EXO] SKIPPED: Remove mobile device data; Reason: No mobile devices found"
-    }
-    else {
-        Write-Host "[EXO] Found mobile device(s). Removing the account from mobile device(s)"
-        foreach ($p in $userPhones) {    
-            Clear-MobileDevice -Identity $p.DistinguishedName -AccountOnly -Confirm:$false
-            Write-Host "[EXO] Initiated email account wipe from mobile device: $($p.FriendlyName),$($p.DeviceModel)"
+    foreach ($UPN in $UserPrincipalName) {
+
+        Write-Host "[EXO] Attempting to locate $($UPN)'s mobile devices to delete data from..."
+        
+        if ($NativeAppOnly) {
+            $userPhones = Get-MobileDevice -Mailbox $UPN | Where-Object { $_.DeviceModel -notLike "*outlook*" }
         }
+        else {
+            $userPhones = Get-MobileDevice -Mailbox $UPN
+        }
+        
+        if ($null -eq $userPhones) {
+            Write-Host "[EXO] SKIPPED: Remove mobile device data; Reason: No mobile devices found"
+        }
+        else {
+            Write-Host "[EXO] Found mobile device(s). Removing the account from mobile device(s)"
+            foreach ($p in $userPhones) {    
+                # Clear-MobileDevice -Identity $p.DistinguishedName -AccountOnly -Confirm:$false
+                Write-Host "[EXO] Initiated email account wipe from mobile device: $($p.Name),$($p.DeviceModel)"
+            }
+        }        
     }
 
 }
 
 function Remove-MobileDevicePartnerships {
 
-    Write-Host "[EXO] Checking for mobile device partnerships..."
+    foreach ($UPN in $UserPrincipalName) {
+
+        Write-Host "[EXO] Checking for mobile device partnerships..."
     
-    if ($NativeAppOnly) {
-        $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | ? { $_.DeviceModel -notLike "*outlook*" }
+        if ($NativeAppOnly) {
+            $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | Where-Object { $_.DeviceModel -notLike "*outlook*" }
         }
         else {
-            $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | ? { $_.DeviceModel -notLike "*outlook*" }
+            $userPhones = Get-MobileDevice -Mailbox $UserPrincipalName | Where-Object { $_.DeviceModel -notLike "*outlook*" }
         }
 
         if ($null -eq $userPhones) {
             Write-Host "[EXO] SKIPPED: Remove mobile device partnerships; Reason: No mobile device partnerships found"
         }
         else {
-            Write-Host "[EXO] Found mobile device(s). Removing the account from mobile device(s)"
+            Write-Host "[EXO] Found mobile device(s). Removing the account from $($UPN)'s mobile device(s)"
             foreach ($p in $userPhones) {    
-                Remove-MobileDevice -Identity $p.DistinguishedName -Confirm:$False 
-                Write-Host "[EXO] Removed mobile device partnership: $($p.FriendlyName),$($p.DeviceModel)"
+                #Remove-MobileDevice -Identity $p.DistinguishedName -Confirm:$False 
+                Write-Host "[EXO] Removed mobile device partnership: $($p.Name),$($p.DeviceModel)"
             }
         }
-    }
 
-    Connect-Modules
-    Remove-MobileDeviceData
-    Write-Host "Waiting 15 minutes before removing mobile device partnerships, please wait ..."
-    Start-Sleep -Seconds 13500
-    Remove-MobileDevicePartnerships
+    }
+}
+
+Connect-Modules
+Remove-MobileDeviceData
+Write-Host "Waiting 15 minutes before removing mobile device partnerships, please wait ..."
+#Start-Sleep -Seconds 13500
+Remove-MobileDevicePartnerships
