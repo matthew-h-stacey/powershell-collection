71e9ab8517dfc6216cd7f30b9e698e729b066207
matthew-h-stacey <matthew.h.stacey@outlook.com>
2023-11-13 11:18:39 -0500

    Added cmd note

--- Windows/Enable-RDP.ps1
+++ Windows/Enable-RDP.ps1
@@ -1,2 +1,13 @@
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+
+<# 
+
+In cmd:
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="remote desktop" new enable=yes
+
+
+#>
