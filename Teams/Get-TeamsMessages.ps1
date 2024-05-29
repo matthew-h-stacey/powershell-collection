dcd287b8db4e561df7e0579619c83ded7a3aa9d8
matthew-h-stacey <matthew.h.stacey@outlook.com>
2024-05-29 17:12:14 -0400

    Add "to" header

--- Teams/Get-TeamsMessages.ps1
+++ Teams/Get-TeamsMessages.ps1
@@ -1,3 +1,7 @@
+# NOTE: API permissions require Application permissions (app reg) - does NOT work with delegated (account) permissions
+# Create an app reg with Chat.ReadWrite.All permissions
+# Connect-mggraph -CertificateThumbprint XXXXXXXXXX -ClientId XXXXXXXXXX -TenantId XXXXXXXXXX
+
 [CmdletBinding(DefaultParameterSetName="All")]
 param (
     # Parameter help description
@@ -21,35 +25,55 @@ param (
     $EndDate
 )
 
-if ($PSCmdlet.ParameterSetName -eq "Range") {
-    # Convert the dates to UTC
-    $StartDateUTC = (Get-Date $StartDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    $EndDateUTC = (Get-Date $EndDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    Write-Host "[START] Initiating script with date range. Converted time range to UTC: $StartDateUTC - $EndDateUTC"
-    # Construct the Uri and apply the date range filter
-    $Filter = "?`$filter=lastModifiedDateTime gt $StartDateUTC and lastModifiedDateTime lt $EndDateUTC"
-    $Uri = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/chats/getAllMessages" + $Filter
-} else {
-    # Construct the Uri
-    $Uri = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/chats/getAllMessages"
+function Get-ChatParticipants {
+    param (
+        [String] $ChatId
+    )
+    $uri = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/chats/$ChatId/members"
+    $response = Invoke-MgGraphRequest -Uri $uri -Method "GET"
+    return $response.value | ForEach-Object { $_.displayName }
 }
 
-$Method = "GET"
-$MSGraphOutput = @()
-$NextLink = $null
+if ($PSCmdlet.ParameterSetName -eq "Range") {
+    # Convert the dates to UTC
+    $startDateUTC = (Get-Date $StartDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+    $endDateUTC = (Get-Date $EndDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+    Write-Host "[START] Initiating script with date range. Converted time range to UTC: $startDateUTC - $endDateUTC"
+    # Construct the Uri and apply the date range filter
+    $filter = "?`$filter=lastModifiedDateTime gt $StartDateUTC and lastModifiedDateTime lt $EndDateUTC"
+    $uri = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/chats/getAllMessages" + $filter
+} else {
+    # Construct the Uri
+    $uri = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/chats/getAllMessages"
+}
+
+$method = "GET"
+$msGraphOutput = @()
+$nextLink = $null
 do {
-    $Uri = if ($NextLink) {
-        $NextLink
+    $uri = if ($nextLink) {
+        $nextLink
     }
     else {
-        $Uri
+        $uri
     }
-    $Response = Invoke-MgGraphRequest -Uri $Uri -Method $Method
-    $Output = $Response.Value
-    $MSGraphOutput += $Output
-    $NextLink = $Response.'@odata.nextLink'
-} until (-not $NextLink)
+    $response = Invoke-MgGraphRequest -Uri $uri -Method $method
+    $output = $response.Value
+    $msGraphOutput += $output
+    $nextLink = $response.'@odata.nextLink'
+} until (-not $nextLink)
 
 # Format and output the chat messages
-$Chats = $MSGraphOutput |  Select-Object  @{Name = "Sent"; Expression = { Get-Date $_.createdDateTime -Format "MM/dd/yyyy HH:mm:ss " } }, @{Name = "From"; Expression = { $_.from.user.displayName } }, @{Name = "Content"; Expression = { $_.body.content } }, @{Name = "ReplyContent"; Expression = { $_.attachments.content } }#| Export-Csv "$PSScriptRoot\ExportedChats.csv" -Append
-$chats | export-csv C:\TempPath\TeamsChatExport_$UserPrincipalName.csv -NoTypeInformation
+$chats = $msGraphOutput | Select-Object -Property (
+    @{Name = "Sent"; Expression = { Get-Date $_.createdDateTime -Format "MM/dd/yyyy HH:mm:ss" } },
+    @{Name = "From"; Expression = { $_.from.user.displayName } },
+    @{Name = "To"; Expression = {
+            $participants = Get-ChatParticipants -ChatId $_.chatId
+            $msgSender = $_.from.user.displayName
+        ($participants | Where-Object { $_ -ne $msgSender }) -join ", "
+        }
+    },
+    @{Name = "Content"; Expression = { $_.body.content } },
+    @{Name = "ReplyContent"; Expression = { $_.attachments.content } }
+)
+$chats | Export-Csv C:\TempPath\TeamsChatExport_$UserPrincipalName.csv -NoTypeInformation
