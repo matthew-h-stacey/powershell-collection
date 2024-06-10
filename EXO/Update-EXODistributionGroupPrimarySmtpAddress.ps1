13fa7d347c8d0ac08e330ae0ca40390e5a578749
matthew-h-stacey <matthew.h.stacey@outlook.com>
2024-06-10 09:37:50 -0400

    Add $CSV param; Variable cleanup

--- EXO/Update-EXODistributionGroupPrimarySmtpAddress.ps1
+++ EXO/Update-EXODistributionGroupPrimarySmtpAddress.ps1
@@ -1,16 +1,21 @@
 ﻿<#
 .SYNOPSIS
-Bulk change the PrimarySmtpAddress of distribution groups to use a new domain suffix. For example, after a domain name change, during a migration project
+Bulk change the PrimarySmtpAddress of distribution groups to use a new domain suffix. For example, after a domain name change or during a migration project
 
 .EXAMPLE
 Update-EXODistributionGroupPrimarySmtpAddress.ps1 -NewVanityDomain contoso.com -ExportPath C:\TempPath
 #>
 
 param(   
-    # This is the new vanity domain to set on all mailboxes (ex: contoso.com)    
+    # This is the new vanity domain to set on all distribution groups (ex: contoso.com)    
     [Parameter(Mandatory = $true)]
     [string]
     $NewVanityDomain,
+
+    # Optionally provide the full path to a CSV instead of applying changes to all distribution groups. The CSV should use "PrimarySmtpAddress" as the header
+    [Parameter(Mandatory = $false)]
+    [string]
+    $CSV,
 
     # Path to export results to
     [Parameter(Mandatory = $true)]
@@ -18,43 +23,60 @@ param(
     $ExportPath
 )
 
-$ExportPath = $($ExportPath.TrimEnd("\")) # trim trailing "\""
-$distis = Get-DistributionGroup -ResultSize Unlimited
-
+$outputFile = "$($ExportPath.TrimEnd("\"))\distiReport.csv"
 $results = New-Object System.Collections.Generic.List[System.Object]
+
+if ( $CSV ) {
+    if ( Test-Path $CSV ) {
+        $distis = Import-Csv -Path $CSV | ForEach-Object { 
+            $filter = "PrimarySmtpAddress -eq '" + $_.PrimarySmtpAddress + "'"
+            Get-DistributionGroup -Filter $filter
+        }
+    } else {
+        Write-Output "[ERROR] Failed to locate CSV at provided path. Please check the path and try again"
+        exit 1
+    }
+
+} else {
+    $distis = Get-DistributionGroup -ResultSize Unlimited
+}
+
 foreach ($disti in $distis) {
 
     # First check to see if the new vanity domain name has already been applied
-    $changeRequired = $disti.PrimarySmtpAddress.Split("@")[1] -ne $NewVanityDomain
+    $currentPrimarySmtpAddress = $disti.PrimarySmtpAddress
+    $changeRequired = $currentPrimarySmtpAddress.Split("@")[1] -ne $NewVanityDomain
     if ( $changeRequired ) {
         # Remove old domain, append new domain
-        $newPrimarySmtpAddress = $disti.PrimarySmtpAddress.Split("@")[0] + "@" + $NewVanityDomain
+        $newPrimarySmtpAddress = $currentPrimarySmtpAddress.Split("@")[0] + "@" + $NewVanityDomain
         # Retrieve original values for disti and store in a custom object
         $groupOutput = [ordered]@{
-            oldPrimarySmtpAddress  = $disti.PrimarySmtpAddress
+            oldPrimarySmtpAddress  = $currentPrimarySmtpAddress
             oldWindowsEmailAddress = $disti.WindowsEmailAddress
             oldEmailAddresses      = ($disti.EmailAddresses) -join ";"
         }
-        Write-Output "[INFO] Changing PrimarySmtpAddress value from: $($disti.PrimarySMTPAddress) -> $newPrimarySmtpAddress"
         try {
-            Set-DistributionGroup -Identity $disti.PrimarySmtpAddress -PrimarySmtpAddress $newPrimarySmtpAddress
+            Set-DistributionGroup -Identity $currentPrimarySmtpAddress -PrimarySmtpAddress $newPrimarySmtpAddress
+            Write-Output "[INFO] Changed PrimarySmtpAddress value from: $currentPrimarySmtpAddress -> $newPrimarySmtpAddress"
             try {
-                $disti = Get-DistributionGroup -Identity $newPrimarySmtpAddress -ErrorAction Stop
+                $distiUpdated = Get-DistributionGroup -Identity $newPrimarySmtpAddress -ErrorAction Stop
             } catch {
                 Write-Output "[ERROR] Unable to locate Distribution Group using new PrimarySmtpAddress $newPrimarySmtpAddress"
             }
             # Retrieve updated values for disti
-            $groupOutput["newPrimarySmtpAddress"] = $disti.PrimarySmtpAddress
-            $groupOutput["newWindowsEmailAddress"] = $disti.WindowsEmailAddress
-            $groupOutput["newEmailAddresses"] = ($disti.EmailAddresses) -join ";"
+            $groupOutput["newPrimarySmtpAddress"] = $distiUpdated.PrimarySmtpAddress
+            $groupOutput["newWindowsEmailAddress"] = $distiUpdated.WindowsEmailAddress
+            $groupOutput["newEmailAddresses"] = ($distiUpdated.EmailAddresses) -join ";"
             $results.Add([PSCustomObject]$groupOutput) 
         } catch {
             Write-Output "[ERROR] Failed to update PrimarySmtpAddress. Error: $($_.Exception.Message)"
         }
+    } else {
+        Write-Output "[INFO] $currentPrimarySmtpAddress - PrimarySmtpAddress is already set to the new vanity domain name"
     }
 }
 
 if ( $results ) { 
-    $results | Export-Csv -Path $ExportPath\distiReport.csv -NoTypeInformation
-    Write-Host "[INFO] Exported results to $ExportPath\distiReport.csv"
+    $results | Export-Csv -Path $outputFile -NoTypeInformation
+    Write-Host "[INFO] Exported results to $outputFile"
 }
