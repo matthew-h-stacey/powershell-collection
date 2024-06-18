2bbe3001765c15f13faa7ae6950363fb85d1c18a
matthew-h-stacey <matthew.h.stacey@outlook.com>
2024-06-18 19:14:25 -0400

    Add local web server w/ authentication example

--- _Snip-its/DownloadFile.ps1
+++ _Snip-its/DownloadFile.ps1
@@ -1,5 +1,18 @@
+# Example: Public download
+
 $Url = "https://portableapps.com/redir2/?a=FirefoxPortable&s=s&d=pa&f=FirefoxPortable_122.0_English.paf.exe"
 $LocalPath = "C:\TempPath"
 $FileName = "FirefoxPortable_120.0.1_English.paf.exe"
 $FilePath = "$LocalPath\$FileName"
 (New-Object System.Net.WebClient).DownloadFile($Url, $FilePath)
+
+# Example: Private download with authentication
+
+$username = "serviceadmin"
+$uri = "http://localhost/reports/example.aspx"
+$securePassword = ConvertTo-SecureString "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+$outputFile = "C:\Scripts\htmlResponse.txt"
+
+$credentials = New-Object System.Management.Automation.PSCredential($username, $securepassword)
+$response = Invoke-WebRequest -Uri $Uri -Credential $credentials
+$response.RawContent | Out-File $outputFile
