3dad5deb3c72793237a9b42719304f03cf5655aa
matthew-h-stacey <matthew.h.stacey@outlook.com>
2024-06-21 08:25:12 -0400

    Snip-it cleanup

--- _Snip-its/FilterOData.ps1
+++ _Snip-its/FilterOData.ps1
@@ -27,12 +27,24 @@ $AADUser = Get-AzureADUser -Filter $filter
 
 # 3b) More involved example
 $AADGroups = (Get-AzureADUserMembership -ObjectId $AADUser.objectID -All:$True) | Where-Object { $_.MailEnabled -eq $False } # All Azure group membership
-foreach ( $g in $AADGroups) { # build up the $excludedGroups array with any dynamic groups
+foreach ( $g in $AADGroups) {
+    # build up the $excludedGroups array with any dynamic groups
     $displayName = $g.DisplayName
     $filter = "startsWith(DisplayName,'" + $displayName + "')"
-    if (((Get-AzureADMSGroup -Filter $filter).GroupTypes) -eq "DynamicMembership") { # If this matches, group is dynamic and should be skipped from the group removal process below
+    if (((Get-AzureADMSGroup -Filter $filter).GroupTypes) -eq "DynamicMembership") {
+        # If this matches, group is dynamic and should be skipped from the group removal process below
         $excludedGroups += $g.DisplayName
     }
 }
 
-
+########################
+# Graph example
+########################
+$params = @{
+    Top    = 1
+    Filter = "IsInteractive eq true and UserPrincipalName eq '$UserPrincipalName'"
+    Select = "CreatedDateTime,Location,IPAddress,IsInteractive"
+}
+$queryString = ($params.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join "&"
+$uri = "https://graph.microsoft.com/beta/auditLogs/signIns?$queryString"
+$lastSignIn = Invoke-MgGraphRequest -Method GET -Uri $uri
