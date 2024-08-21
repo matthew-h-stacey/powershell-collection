27d710a32e139d42fe28ee414abda3488ada500c
matthew-h-stacey <matthew.h.stacey@outlook.com>
2024-08-21 17:05:27 -0400

    Comment change

--- Windows/Fix-TLS.ps1
+++ Windows/Fix-TLS.ps1
@@ -1,7 +1,7 @@
 # ex: https://www.alitajran.com/unable-to-install-nuget-provider-for-powershell/
 # Also PS reporting modules like "AzureADPreview" cannot be found
 
-# 1-off (?)
+# 1-off
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # OR, registry approach. Force .NET to use secure encryption. 32 and 64-bit
